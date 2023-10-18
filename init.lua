014461bd73d9ba1199bd9a7a45fc13c3afd04c64
Jacob Atnip <jatnip64@gmail.com>
2023-10-17 19:49:12 -0500

    added barbar

--- init.lua
+++ init.lua
@@ -42,7 +42,19 @@ plugins = {
         config = function()
             require("ibl").setup()
         end
-    }
+    },
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {'nvim-tree/nvim-web-devicons'},
+        config = function()
+            require('bufferline').setup {
+                animation = false,
+                icons = {
+                    buffer_index = true,
+                },
+            }
+        end,
+    },
 }
 require("lazy").setup(plugins)
 
@@ -62,5 +74,16 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>t', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>c', ':bd<Enter>', {})
+vim.keymap.set('n', '<leader>1' ,':BufferGoto 1<Enter>', {}) 
+vim.keymap.set('n', '<leader>2' ,':BufferGoto 2<Enter>', {})
+vim.keymap.set('n', '<leader>3' ,':BufferGoto 3<Enter>', {})
+vim.keymap.set('n', '<leader>4' ,':BufferGoto 4<Enter>', {})
+vim.keymap.set('n', '<leader>5' ,':BufferGoto 5<Enter>', {})
+vim.keymap.set('n', '<leader>6' ,':BufferGoto 6<Enter>', {})
+vim.keymap.set('n', '<leader>7' ,':BufferGoto 7<Enter>', {})
+vim.keymap.set('n', '<leader>8' ,':BufferGoto 8<Enter>', {})
+vim.keymap.set('n', '<leader>9' ,':BufferGoto 9<Enter>', {})
+
 
 vim.keymap.set('n', '<leader>p',":b#<Enter>", {})
